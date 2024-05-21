325a74fce87c1e09b566627a08a869f2cf8d6e4b
Ryan Haney <25014745+thathaneydude@users.noreply.github.com>
2024-05-21 16:57:19 -0500

    fixing event grid access

--- service_bus.tf
+++ service_bus.tf
@@ -7,6 +7,7 @@ resource "azurerm_servicebus_namespace" "state_change_bus" {
   minimum_tls_version = "1.2"
 
   network_rule_set {
+    trusted_services_allowed = true
     ip_rules = [
       azurerm_container_app_environment.enrichment_app_env.static_ip_address
     ]
