29c6ff6e9d5325e13b3312f566d64ed5f3e735aa
Ryan Haney <25014745+thathaneydude@users.noreply.github.com>
2024-05-21 16:47:24 -0500

    Storage Account Access & ServiceBus IP Rules (#3)

--- service_bus.tf
+++ service_bus.tf
@@ -1,14 +1,15 @@
 resource "azurerm_servicebus_namespace" "state_change_bus" {
-  location                      = var.location
-  name                          = var.service_bus_name
-  resource_group_name           = var.resource_group_name
-  sku                           = "Standard"
-  public_network_access_enabled = false
-  local_auth_enabled            = false
-  minimum_tls_version           = "1.2"
+  location            = var.location
+  name                = var.service_bus_name
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
+  local_auth_enabled  = false
+  minimum_tls_version = "1.2"
 
   network_rule_set {
-    trusted_services_allowed = true
+    ip_rules = [
+      azurerm_container_app_environment.enrichment_app_env.static_ip_address
+    ]
   }
 
   tags = var.tags
